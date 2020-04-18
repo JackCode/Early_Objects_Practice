6c433fc84b1ca07a80becc2eae438529a5fd4951
Jerod <epicrijks@gmail.com>
2020-04-18 18:05:35 -0400

    Init Commit

---
+++ ch_10_pointers/8_days_in_current_month/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int month, year;
+    bool leapYear;
+
+    do
+    {
+        cout << "Enter month and year (MM YY) or (0 0) to quit: ";
+        cin >> month >> year;
+
+    } while (month < 0 || month > 12 || );
+    
+
+    return 0;
+}
