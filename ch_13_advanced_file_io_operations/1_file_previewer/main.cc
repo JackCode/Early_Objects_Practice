378923086cd6a3c29ff4a318365af6e722be39b8
Jerod <epicrijks@gmail.com>
2020-04-19 23:50:12 -0400

    Init Commit

---
+++ ch_13_advanced_file_io_operations/1_file_previewer/main.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+  const int NUM_LINES_TO_PRINT = 10,
+            MAX_LINE_SIZE = 81;
+
+  std::string fileName;
+  char lineFromFile[MAX_LINE_SIZE];
+  
+  std::cout << "Enter file name: ";
+  getline(std::cin, fileName);
+
+  std::ifstream inputFile(fileName);
+  if (!inputFile) {
+    std::cout << "Error opening file.\n";
+    return 0;
+  }
+
+  std::cout << "\n";
+
+  for (int i = 0; i < NUM_LINES_TO_PRINT; i++) {
+    inputFile.getline(lineFromFile, 81);
+    
+    if (inputFile.fail()) {
+      std::cout << "\n-- END OF FILE --\n";
+      return 0;
+    } else {
+      std::cout << lineFromFile << "\n";
+    }
+  }
+
+  std::cout << std::endl;
+
+  return 0;
+}
