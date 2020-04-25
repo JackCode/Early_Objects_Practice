11dd3dc2ec5e60f06ee22c00f78beb5304a00967
Jerod <epicrijks@gmail.com>
2020-04-25 19:16:04 -0400

    Replaced pointer notation with subclass instance.

--- ch_15_polymorphism_virtual_functions/2_analysis_of_quicksort/main.cc
+++ ch_15_polymorphism_virtual_functions/2_analysis_of_quicksort/main.cc
@@ -3,7 +3,7 @@
 #include "QuickSort.h"
 
 int main() {
-  AbstractSort *ptr = new QuickSort;
+  QuickSort qs;
   const int ARRAY_SIZE = 10;
   int array[ARRAY_SIZE] {17, 53, 9, 2, 30, 1, 82, 64, 26, 5};
 
@@ -12,13 +12,13 @@ int main() {
 
   std::cout << "\n";
 
-  ptr->sort(array, ARRAY_SIZE);
+  qs.sort(array, ARRAY_SIZE);
 
   for (int i = 0; i < 10; i++)
   std::cout << array[i] << " ";
 
   std::cout << "\n";
-  std::cout << "Number of comparisons: " << ptr->get_num_compares() << "\n";
+  std::cout << "Number of comparisons: " << qs.get_num_compares() << "\n";
 
   return 0;
 }
