ad06206d162bde5f1b4dc37a07b20155290dff2c
Jerod <epicrijks@gmail.com>
2020-04-25 19:17:12 -0400

    Replaced pointer notation with subclass instance.

--- ch_15_polymorphism_virtual_functions/3_sequence_sum/main.cc
+++ ch_15_polymorphism_virtual_functions/3_sequence_sum/main.cc
@@ -4,14 +4,14 @@
 #include "TriangularNumbers.h"
 
 int main() {
-  AbstractSeq *odd = new OddNumbers;
-  AbstractSeq *tri = new TriangularNumbers;
+  OddNumbers odd;
+  TriangularNumbers tri;
 
-  odd->printSeq(0, 10);
-  std::cout << "\nSum of above numbers: " << odd->sumSeq(0, 10) << "\n\n";
+  odd.printSeq(0, 10);
+  std::cout << "\nSum of above numbers: " << odd.sumSeq(0, 10) << "\n\n";
 
-  tri->printSeq(0, 10);
-  std::cout << "\nSum of above numbers: " << tri->sumSeq(0, 10) << "\n";
+  tri.printSeq(0, 10);
+  std::cout << "\nSum of above numbers: " << tri.sumSeq(0, 10) << "\n";
 
 
   return 0;
