d0a8815eb56a7830048f4eae6184d62b03b946d8
Jerod <epicrijks@gmail.com>
2020-04-25 19:14:51 -0400

    Replaced pointer notation with call to subclass

--- ch_15_polymorphism_virtual_functions/1_analysis_sorting_algorithms/main.cc
+++ ch_15_polymorphism_virtual_functions/1_analysis_sorting_algorithms/main.cc
@@ -5,20 +5,20 @@
 int main() {
     const int ARRAY_SIZE = 10;
     int array[] = {3, 90, 54, 13, 1, 76, 84, 32, 12, 99};
-    AbstractSort *ptr = new BubbleSort;
+    BubbleSort bs;
 
     for (int i = 0; i < ARRAY_SIZE; i++)
         std::cout << array[i] << " ";
 
     std::cout << "\n";
 
-    ptr->sort(array, ARRAY_SIZE);
+    bs.sort(array, ARRAY_SIZE);
 
     for (int i = 0; i < ARRAY_SIZE; i++)
         std::cout << array[i] << " ";
 
     std::cout << "\n";
-    std::cout << "Number of comparisons: " << ptr->get_num_compares() << "\n";
+    std::cout << "Number of comparisons: " << bs.get_num_compares() << "\n";
 
     return 0;
 }
