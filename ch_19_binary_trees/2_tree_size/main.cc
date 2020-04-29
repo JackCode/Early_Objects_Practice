671be16f528c6c43360ce50763ddd157a8605b12
Jerod <epicrijks@gmail.com>
2020-04-29 12:29:49 -0400

    Init Commit

---
+++ ch_19_binary_trees/2_tree_size/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "BinaryTree.h"
+
+int main() {
+  jw::BinaryTree tree;
+  std::vector<double> values;
+
+  try {
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(8);
+    tree.insert(12);
+    tree.insert(9);
+  } catch (jw::BinaryTree::TreeMemError) {
+    std::cout << "ERROR: Memory error.\n";
+  }
+
+  tree.inorder(values);
+
+  std::cout << "Inorder: ";
+  for (const double &v : values) {
+    std::cout << v << " ";
+  }
+
+  std::cout << "\n";
+  std::cout << "Size: " << tree.size() << "\n\n";
+
+  return 0;
+}
