3adff9b6de1570f38bc95afbe59efe952796ec6e
Jerod <epicrijks@gmail.com>
2020-04-29 12:45:16 -0400

    Init Commit

---
+++ ch_19_binary_trees/3_leaf_counter/BinaryTree.h
@@ -0,0 +1,44 @@
+#ifndef BINARY_TREE_H_
+#define BINARY_TREE_H_
+
+#include <vector>
+
+namespace jw {
+
+class BinaryTree {
+ public:
+  BinaryTree();
+  ~BinaryTree();
+
+  void insert(double value);
+  bool search(double value);
+  void inorder(std::vector<double> &v);
+  int size();
+  int leafCount();
+
+  // Exception Classes
+  class TreeMemError {};
+
+ private:
+  class TreeNode {
+    friend class BinaryTree;
+    double value;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(double value, TreeNode* left, TreeNode* right) {
+      this->value = value;
+      this->left = left;
+      this->right = right;
+    }
+  };
+  TreeNode* root;
+  bool search(double value, TreeNode* tree);
+  void inorder(std::vector<double> &v, TreeNode* tree);
+  int size(TreeNode* tree);
+  int leafCount(TreeNode* tree);
+};
+
+}
+
+#endif
